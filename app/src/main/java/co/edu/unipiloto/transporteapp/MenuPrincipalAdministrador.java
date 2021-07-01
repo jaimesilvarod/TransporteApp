437b9b663376101fa827beb80b3cf192681dd980
jaimesilvarod <jaimesilvarod@hotmail.com>
2021-07-01 17:53:33 -0500

    Proyecto TransporteApp

---
+++ app/src/main/java/co/edu/unipiloto/transporteapp/MenuPrincipalAdministrador.java
@@ -0,0 +1,14 @@
+package co.edu.unipiloto.transporteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuPrincipalAdministrador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal_administrador);
+    }
+}
