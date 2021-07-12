8018e338fe1fc2d4fbb71873d492b8cbdfb40f69
jaimesilvarod <jaimesilvarod@hotmail.com>
2021-07-12 03:42:23 -0500

    Agrego plantillas registro Propietarios y de Usuarios

--- app/src/main/java/co/edu/unipiloto/transporteapp/RegistroUsuario.java
+++ app/src/main/java/co/edu/unipiloto/transporteapp/RegistroUsuario.java
@@ -3,12 +3,31 @@ package co.edu.unipiloto.transporteapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegistroUsuario extends AppCompatActivity {
+
+    EditText nombresRegistrados, apellidosRegistrados, correoRegistrado, contrasenaRegistrada, confirmaContrasena;
+    Button mregistroUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usuario);
+
+        nombresRegistrados = findViewById(R.id.nombreUsuarioEditText);
+        apellidosRegistrados = findViewById(R.id.apellidosUsuarioEditText);
+        correoRegistrado = findViewById(R.id.correoElectronicoEditText);
+        contrasenaRegistrada = findViewById(R.id.contrasenaUsuarioEditText);
+        confirmaContrasena = findViewById(R.id.confirmaContrasenaUsuarioEditText);
+        mregistroUsuario = findViewById(R.id.btnRegistroUsuario);
     }
+
+    private void validacionDatosRegistrados(View view) {
+
+    }
+
 }
