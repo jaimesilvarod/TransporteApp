c9a52d43e807f5ac019d5f3179c70577f18965c3
jaimesilvarod <jaimesilvarod@hotmail.com>
2021-07-21 07:04:17 -0500

    Menú Propietario corregido y consulta de vehiculos a su nombre

---
+++ app/src/main/java/co/edu/unipiloto/transporteapp/MenuPrincipalPropietario.java
@@ -0,0 +1,66 @@
+package co.edu.unipiloto.transporteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MenuPrincipalPropietario extends AppCompatActivity {
+
+    TextView view, botonVehiculos;
+    Toolbar myToolbar;
+    String correoUsuario;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal_propietario);
+        myToolbar = findViewById(R.id.toolbarComerciante);
+        setSupportActionBar(myToolbar);
+        view = findViewById(R.id.tituloLogueadoTextView);
+        correoUsuario = getIntent().getStringExtra("correoLogueado");
+        String textoInicial = "Bienvenido: " + correoUsuario;
+        view.setText(textoInicial);
+        botonVehiculos = findViewById(R.id.btnxVehiculo);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.cerrarSesion:
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.putExtra("origen", "cerrarSesion");
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void mostrarVehiculos(View view) {
+        Intent intent = new Intent(this, VehiculosasuNombre.class);
+        intent.putExtra("correoUsuario", correoUsuario);
+        startActivity(intent);
+    }
+
+    public void registrarVehiculos(View view) {
+        Intent intent = new Intent(this, RegistrarVehiculo.class);
+        intent.putExtra("correoUsuario", correoUsuario);
+        startActivity(intent);
+    }
+}
