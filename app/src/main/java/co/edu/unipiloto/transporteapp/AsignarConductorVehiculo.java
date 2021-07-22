27a785d40598f68348fd735b9ad9162787fd9232
jaimesilvarod <jaimesilvarod@hotmail.com>
2021-07-22 17:47:14 -0500

    - Spinner

--- app/src/main/java/co/edu/unipiloto/transporteapp/AsignarConductorVehiculo.java
+++ app/src/main/java/co/edu/unipiloto/transporteapp/AsignarConductorVehiculo.java
@@ -5,15 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.protobuf.LazyStringArrayList;
 
 import java.util.ArrayList;
 
@@ -23,9 +27,9 @@ public class AsignarConductorVehiculo extends AppCompatActivity {
 
     String correoUsuarioReg = "";
     TextView view;
-    Spinner conductoresPool;
-    public ArrayList<String> lenguajes = new ArrayList<>();
-    Integer posicionArreglo = 0;
+    Spinner conductoresPool, vehiculosPool;
+    ArrayList<String> conductores = new ArrayList<>();
+    ArrayList<String> vehiculos = new ArrayList<>();
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
@@ -33,6 +37,7 @@ public class AsignarConductorVehiculo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asignar_conductor_vehiculo);
         conductoresPool = findViewById(R.id.spinnerConductores);
+        vehiculosPool = findViewById(R.id.spinnerVehiculos);
         correoUsuarioReg = getIntent().getStringExtra("correoUsuario");
         view = findViewById(R.id.textViewTituloAsignarCVehiculo);
         String textoInicial = "Bienvenido: " + correoUsuarioReg;
@@ -48,9 +53,7 @@ public class AsignarConductorVehiculo extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 Log.d(TAG, document.getId() + " => " + document.getData());
-                                lenguajes.set(posicionArreglo, document.getString("nombres") + " " + document.getString("apellidos"));
-                                //Log.d(TAG, document.getId() + " => " + document.getData() + " " + lenguajes.get(posicionArreglo));
-                                posicionArreglo++;
+                                conductores.add(document.getString("nombres") + " " + document.getString("apellidos"));
                             }
                         } else {
                             Log.d(TAG, "Error getting documents: ", task.getException());
@@ -58,8 +61,25 @@ public class AsignarConductorVehiculo extends AppCompatActivity {
                     }
                 });
 
-        conductoresPool.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item,lenguajes));
+        conductoresPool.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,conductores));
 
-
+        //Vamos a buscar los conductores y asignarlos a la posición en el arreglo
+        db.collection("vehiculos")
+                .whereEqualTo("propietario", correoUsuarioReg)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                vehiculos.add(document.getString("marca") + " " + document.getString("placa"));
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+        vehiculosPool.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,vehiculos));
     }
 }
